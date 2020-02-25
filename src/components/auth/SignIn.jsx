19e14d00a53b57601590ca35c7cc4ba30fe19c89
superbrobenji <bennie.h.swanepoel@gmail.com>
2020-02-25 18:21:54 +0200

    oops

--- src/components/auth/SignIn.jsx
+++ src/components/auth/SignIn.jsx
@@ -21,7 +21,6 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import TextField from '@material-ui/core/TextField';
 
 const mapDispatchToProps = dispatch => ({
 	signin: (email, password, callback) =>
