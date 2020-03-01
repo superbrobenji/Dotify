7bd389fd17eb10c52d1b36d38635525b31d2b5ad
superbrobenji <bennie.h.swanepoel@gmail.com>
2020-03-01 19:00:02 +0200

    upload songs and song streaming added

--- src/actions/songs.js
+++ src/actions/songs.js
@@ -3,8 +3,11 @@ import {
 	LOADING_FINISH,
 	LOAD_SONGS,
 	LOAD_SONGS_ERROR,
+	UPLOAD_SONG,
+	UPLOAD_SONG_ERROR,
 } from './types';
 import axios from 'axios';
+import firebase from '../services/firebase';
 
 export const getAlbumSongs = (uid, currentAlbum) => async dispatch => {
 	dispatch({
@@ -28,4 +31,66 @@ export const getAlbumSongs = (uid, currentAlbum) => async dispatch => {
 			});
 			dispatch({ LOAD_SONGS_ERROR });
 		});
+};
+
+export const uploadSong = (song, currentAlbum, uid) => async dispatch => {
+	let songUrl = '';
+	dispatch({
+		type: LOADING_ARTIST,
+	});
+	console.log(song, uid);
+
+	const storageRef = firebase.storage().ref();
+
+	const metadata = {
+		contentType: song.type,
+	};
+
+	storageRef
+		.child(`/artist_ ${uid}/albums/album_${currentAlbum}/songs/${song.name}`)
+		.put(song, metadata)
+		.then(snap => {
+			snap.ref.getDownloadURL().then(url => {
+				const songName = song.name;
+				songUrl = url;
+				console.log(url);
+				axios
+					.post(
+						`https://us-central1-dotify-eb26e.cloudfunctions.net/api/uploadsong`,
+						{ songUrl, currentAlbum, songName },
+					)
+					.then(() => {
+						reloadSongs(currentAlbum);
+						dispatch({
+							type: UPLOAD_SONG,
+							payload: songUrl,
+						});
+					})
+					.catch(err => {
+						console.error(err);
+						dispatch({ type: UPLOAD_SONG_ERROR });
+					});
+			});
+		});
+
+	const reloadSongs = currentAlbum => {
+		axios
+			.get(
+				'https://us-central1-dotify-eb26e.cloudfunctions.net/api/getalbumsongs',
+				{ params: { currentAlbumID: currentAlbum } },
+			)
+			.then(res => {
+				console.log(res.data);
+				dispatch({ type: LOAD_SONGS, payload: res.data });
+				dispatch({
+					type: LOADING_FINISH,
+				});
+			})
+			.catch(err => {
+				dispatch({
+					type: LOADING_FINISH,
+				});
+				dispatch({ LOAD_SONGS_ERROR });
+			});
+	};
 };
--- src/components/Songs.jsx
+++ src/components/Songs.jsx
@@ -3,35 +3,99 @@ import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import requireAuth from './hoc/requireAuth';
 import ReactAudioPlayer from 'react-audio-player';
-
 import Navigation from './Navigation';
+import { uploadSong } from '../actions/songs';
+
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+
 const mapStateToProps = state => ({
 	isLoaded: state.firebaseReducer.auth.isLoaded,
 	createdProfile: state.user.createdProfile,
 	songs: state.songs,
 });
 
-const mapDispatchToProps = dispatch => ({});
+const useStyles = makeStyles({
+	root: {
+		minWidth: 275,
+		width: '150px',
+		height: '80px',
+		marginTop: '2rem',
+	},
+	bullet: {
+		display: 'inline-block',
+		margin: '0 2px',
+		transform: 'scale(0.8)',
+	},
+	title: {
+		fontSize: 14,
+	},
+	pos: {
+		marginBottom: 12,
+	},
+	input: {
+		display: 'none',
+	},
+});
+
+const mapDispatchToProps = dispatch => ({
+	uploadSong: (song, currnetAlbum, uid) =>
+		dispatch(uploadSong(song, currnetAlbum, uid)),
+});
 
 const Songs = props => {
+	const classes = useStyles();
 	const [songComps, setSongComps] = useState([]);
 	useEffect(() => {
+		let songComponents = [];
 		props.songs.forEach(song => {
-			handleState(song);
+			songComponents.push(
+				<li key={song.id}>
+					<p>{song.songName}</p>
+					<ReactAudioPlayer src={song.songUrl} controls />
+				</li>,
+			);
+			handleState(songComponents);
 		});
 	}, [props.songs]);
 
 	const handleState = song => {
-		setSongComps(
-			<li key={song.id}>
-				<p>{song.songName}</p>
-				<ReactAudioPlayer src={song.songUrl} controls />
-			</li>,
+		setSongComps(song);
+	};
+
+	const handleSongChange = async event => {
+		const song = event.target.files[0];
+
+		props.uploadSong(
+			song,
+			props.location.state.currentAlbum.id,
+			props.location.state.uid,
 		);
 	};
+
 	return (
 		<div>
 			<Navigation />
+			{props.location.state.uid === props.location.state.currentAlbum.artist ? (
+				<div>
+					<input
+						accept='audio/*'
+						className={classes.input}
+						id='contained-button-file'
+						type='file'
+						onChange={handleSongChange}
+					/>
+					<label htmlFor='contained-button-file'>
+						<Button variant='contained' color='primary' component='span'>
+							Upload song
+						</Button>
+					</label>
+				</div>
+			) : (
+				<div></div>
+			)}
 			<ul>{songComps}</ul>
 		</div>
 	);
