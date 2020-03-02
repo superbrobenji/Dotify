05efe75d535b61c8aa073a1a262e480777668364
superbrobenji <bennie.h.swanepoel@gmail.com>
2020-03-02 18:56:26 +0200

    added loading anim

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,5 +1,22 @@
 import React from 'react';
-import CircularProgress from '@material-ui/core/CircularProgress';
+import styles from '../loadercss/loader.module.css';
 
-const Loader = () => <CircularProgress />;
+import { ThemeProvider } from '@material-ui/core/styles';
+import { useStyles, theme } from '../MaterialTheme/globalTheme';
+
+const Loader = () => {
+	const classes = useStyles();
+
+	return (
+		<ThemeProvider theme={theme}>
+			<div className={classes.body}>
+				<div className={styles.ldsripple}>
+					<div></div>
+					<div></div>
+				</div>
+			</div>
+		</ThemeProvider>
+	);
+};
+
 export default Loader;
