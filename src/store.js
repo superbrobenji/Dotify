00cb2f963b6c614dc326d8732dfce93bbf21c9e8
superbrobenji <bennie.h.swanepoel@gmail.com>
2020-03-03 14:45:40 +0200

    added state persistance

--- src/store.js
+++ src/store.js
@@ -2,13 +2,25 @@ import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
 import rootReducer from './reducers/rootReducers';
 import { composeWithDevTools } from 'redux-devtools-extension';
-
-const initialState = {};
+import { loadState, saveState } from './localStorage';
+import throttle from 'lodash.throttle';
 
 export default () => {
-	return createStore(
+	const persistedState = loadState();
+
+	const store = createStore(
 		rootReducer,
-		initialState,
+		persistedState,
 		composeWithDevTools(applyMiddleware(reduxThunk)),
 	);
+
+	store.subscribe(
+		throttle(() => {
+			saveState({
+				user: store.getState().user,
+			});
+		}, 1000),
+	);
+
+	return store;
 };
